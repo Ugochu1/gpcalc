2a9d3d2d7256f4e335ffed13c931ef6655312be8
Olinya Ugochukwu <olinyaugochukwu1@gmail.com>
2023-05-04 16:52:42 +0100

    created link to database with mongoose

---
+++ lib/mongodb.ts
@@ -0,0 +1,39 @@
+import { Schema, model, models, connect } from "mongoose";
+
+// connecting to mongoose (Get database url from .env.local)
+const url: string = process.env.DATABASE_URL!;
+
+export const run_database = async () => {
+  // create a connection
+  const connection = await connect(url).catch((err) => console.log(err));
+  console.log("Mongoose Connection Established.");
+
+  // create our schemas now
+  interface Semester {
+    course: string;
+    score: number;
+    grade?: string;
+  }
+
+  interface Record {
+    title: string;
+    last_modified: string;
+    semester: Semester[];
+  }
+
+  interface IUser {
+    username: string;
+    password: string;
+    records: Record[];
+  }
+
+  const userSchema = new Schema<IUser>({
+    username: String,
+    password: String,
+    records: Array,
+  });
+
+  const User = models.User || model<IUser>("User", userSchema);
+
+  return { connection, User };
+};
---
+++ pages/api/signup.ts
@@ -0,0 +1,6 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { run_database } from "@/lib/mongodb";
+
+export default async function signup(req: NextApiRequest, res: NextApiResponse) {
+  
+}
