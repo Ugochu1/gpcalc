8632694b6acd2da6b614e48d97fc1945626b22f1
Olinya Ugochukwu <olinyaugochukwu1@gmail.com>
2023-07-18 03:01:51 +0100

    modified next.config.js file

--- next.config.js
+++ next.config.js
@@ -3,4 +3,17 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
+const webpack = require("webpack");
+
+const { parsed: myEnv } = require("dotenv").config({
+  path: "/",
+});
+
+module.exports = {
+  webpack(config) {
+    config.plugins.push(new webpack.EnvironmentPlugin(myEnv));
+    return config;
+  },
+};
+
 module.exports = nextConfig
