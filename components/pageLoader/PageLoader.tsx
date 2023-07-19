eb86877f81144f8fea2eaf8c35033adceddec311
Olinya Ugochukwu <olinyaugochukwu1@gmail.com>
2023-07-19 01:20:41 +0100

    Changed select color

--- components/pageLoader/PageLoader.tsx
+++ components/pageLoader/PageLoader.tsx
@@ -3,7 +3,7 @@ import Logo from "../logo/Logo";
 import styles from "./PageLoader.module.scss";
 
 export const Loading: FC = () => (
-  <div className="h-full fixed top-0 right-3 flex items-center">
+  <div className="h-full absolute top-0 right-3 flex items-center">
     <div className={styles.loading} />
   </div>
 );
